7b42327cd5671af14c488c8ebdec1bd2f2b4b836
sully10024 <clay@Clays-MacBook-Pro.local>
2017-01-28 16:11:35 -0500

    There was this really bad bug, so I fixed it
    This reverts commit 93f6847581d76eb196e471eea0378cb5b2887861.
    
    # Conflicts:
    #	Geotify/AddGeotificationViewController.swift
    #	Geotify/Base.lproj/Main.storyboard

--- Geotify/AddGeotificationViewController.swift
+++ Geotify/AddGeotificationViewController.swift
@@ -26,11 +26,11 @@ class AddGeotificationViewController: UITableViewController {
     addButton.isEnabled = false
   }
 
-  // updates the label to the right of the radius slider
+  
   @IBAction func radiusSliderDidChange(_ sender: Any) {
     let sliderValueAsInt = Int(radiusSlider.value)
     
-    if sliderValueAsInt < 1000 {
+    if sliderValueAsInt < 500 {
       radiusLabel.text = "\(sliderValueAsInt) m"
     } else {
       radiusLabel.text = (NSString(format: "%.1f", (radiusSlider.value)/1000) as String) + " km"
@@ -66,7 +66,7 @@ class AddGeotificationViewController: UITableViewController {
     mapView.removeAnnotations(mapView.annotations)
   }
   
-  @IBAction func searchTextFieldDidReturn(_ sender: AnyObject) {
+  @IBAction func textFieldDidReturn(_ sender: AnyObject) {
     _ = sender.resignFirstResponder()
     mapView.removeAnnotations(mapView.annotations)
     self.performSearch()
